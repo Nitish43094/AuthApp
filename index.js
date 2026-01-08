22c388540d5028a77d8b6615f7cf965ea1ca1fc6
Nitish43094 <www.nitish93369@gmail.com>
2026-01-08 11:56:46 +0545

    improver database call

--- index.js
+++ index.js
@@ -6,11 +6,27 @@ const PORT = process.env.PORT || 4000
 const user = require('./route/user')
 
 app.use(express.json())
-dbConnection();
+let dbReadyPromise;
+
+if (!dbReadyPromise) {
+    dbReadyPromise = dbConnection();
+}
+app.use(async (req, res, next) => {
+    try {
+        await dbReadyPromise; // wait only first time
+        next();
+    } catch (err) {
+        res.status(500).json({
+            success: false,
+            message: "Database unavailable",
+        });
+    }
+});
 app.use('/api/v1',user)
 app.get('/',(req,res)=>{
     res.send(`<h1>Ther Auth Server Start</h1>`)
 })
+
 app.listen(PORT,()=>{
     console.log("The server started at Port no - ",PORT)
 })
